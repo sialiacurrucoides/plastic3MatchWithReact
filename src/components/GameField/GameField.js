fe84d7b20e5c899305c5f63f69503e16b35e7411
Simon Júlia <simon.julia@nexogen.hu>
2021-05-29 15:11:43 +0200

    small field gen bug fix

--- src/components/GameField/GameField.js
+++ src/components/GameField/GameField.js
@@ -1,11 +1,28 @@
+import React, {useState} from 'react';
 import styles from './GameField.module.scss';
 import { generateList } from './utils/generateList';
 import Tile from './Tile/Tile';
+import { tileStates } from './constants/constants';
+import detectPatterns from './utils/detectPatterns';
+import sumPoints from './utils/sumPoints';
+
+let initialField = generateList();
+while (Number(sumPoints(detectPatterns(initialField))) !== 0){
+    initialField = generateList();
+}
 
 const GameField = () => {
-    const initialField = generateList(); console.log(initialField);
+    const [field, setField] = useState(initialField);
+    console.log("points", sumPoints(field), "field", field);
+
     return (<div className={styles.gameField}>
-            {initialField.map((tile, index) => <Tile key={`tile${index}`} position={index} tileValue={tile.value} tileState={tile.state} />)}
+            {field.map((tile, index) => <Tile 
+            key={`tile${tile.position}`} 
+            position={tile.position} 
+            tileValue={tile.value} 
+            tileState={tile.pointValue > 0 ? tileStates[2] : tileStates[0]}
+            onSwitch={setField}
+            />)}
         </div>);
 };
 
